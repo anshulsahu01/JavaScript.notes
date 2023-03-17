221e07fbb07930c0315624d1d0f895d251da59cc
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-03-17 10:37:40 +0530

    Add files via upload

---
+++ Condition and loops.js
@@ -0,0 +1,71 @@
+ //  variable and Values ( number, String)
+   // operators
+      // Condition and Loops
+
+
+//   let age = 16;
+//   if ( age >= 18) {
+//       console.log("you are allowed");
+//     }
+//     else {
+//         console.log("you are not allowed");
+//     }
+
+
+// let signal = "red";
+// if(signal =="red"){
+//     console.log("red => STOP");
+// }//condition 1
+// else if(signal =="yellow"){
+//     console.log("yellow => go slow");
+// }//condition 2
+// else if ( signal =="green"){
+
+//     console.log(" green => go fast");
+// } //consiton 3 
+// else{
+//     console.log("invalid");
+// }//Last
+
+
+// switch case
+// let user ="Admin";
+// switch(user){
+//     case"admin":
+//     console.log("he is admin");
+//     break;
+// }
+
+
+
+// Loop
+// do whilw, while, for
+
+// for ( let i=0; i<5; i++){
+//     console.log(i);
+// }
+
+
+// while (initialization, condition, incri/ decriment)
+
+// let i = 0;
+
+// while(i>5){
+//     console.log(i);
+//     i++;
+// }
+
+
+//  do while  ek baar ti chalni chahiye
+
+let i = 0;
+do{
+    console.log("hello");
+    i++;
+}while (i<5);
+
+
+
+
+// ternary condition
+// ?  true : false
